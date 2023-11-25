06801ce1dc44756a2ac4ef2d1714021a07ce1e6f
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-11-25 21:36:10 +0300

    Completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_events = [num for num in num_list if num % 2 == 0]
+    return num_events
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence = [sentence + '!' for sentence in sentence_list ]
+
+    return exclamation_sentence 
